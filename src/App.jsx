4084a84ec80be33ce2c50c6982bd2782acfadb0d
Mudassir Ansari <mudassirimranansari@gmail.com>
2023-06-24 16:33:19 +0530

    Done Routing Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import Navbar from "./Navbar"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <Navbar />
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+          </Routes>
+        </Router>
+      </div>
     </>
   )
 }
